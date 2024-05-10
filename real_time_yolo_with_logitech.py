3960aa3ea9180a37f9e3e93b268315362eccd80a
Enes Güngör <66514755+allefenes@users.noreply.github.com>
2024-05-10 22:05:20 +0300

    upload system

---
+++ real_time_yolo_with_logitech.py
@@ -0,0 +1,93 @@
+# python real_time_yolo.py
+import cv2
+import numpy as np
+import time
+
+# Load Yolo
+net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# Configure Camera
+cap = cv2.VideoCapture(2)
+
+font = cv2.FONT_HERSHEY_PLAIN
+starting_time = time.time()
+frame_id = 0
+while True:
+    _, frame = cap.read()
+    frame_id += 1
+
+    height, width, channels = frame.shape
+
+    # Detecting objects
+    blob = cv2.dnn.blobFromImage(frame, 0.00392, (416,416), (0, 0, 0), True, crop=False)
+
+    net.setInput(blob)
+    outs = net.forward(output_layers)
+
+    # Showing informations on the screen
+    class_ids = []
+    confidences = []
+    boxes = []
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.2:
+                # Object detected
+                center_x = int(detection[0] * width)
+                center_y = int(detection[1] * height)
+                w = int(detection[2] * width)
+                h = int(detection[3] * height)
+
+                # Rectangle coordinates
+                x = int(center_x - w / 2)
+                y = int(center_y - h / 2)
+
+                boxes.append([x, y, w, h])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.8, 0.3)
+
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]  
+            #(x, y) = (boxes[i][0], boxes[i][1])
+            #(w, h) = (boxes[i][2], boxes[i][3])
+
+            label = str(classes[class_ids[i]])
+            confidence = confidences[i]
+            color = colors[class_ids[i]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+
+            #orta nokta hesaplama
+            a = x + (w/2)
+            b = y + (h/2)
+            cv2.circle(frame, ((int(a)), (int(b))), 1, (0, 0, 255), 6)
+
+            cv2.putText(frame, label + " " + str(round(confidence, 2)), (x, y + 30), font, 3, color, 3)
+
+
+    #Camera Middle Point
+    camWidth = cap.get(3)
+    camHeight = cap.get(4)
+    camera_midpoint = (int(camWidth / 2), int(camHeight / 2))
+    cv2.circle(frame, camera_midpoint, 1, (0, 255, 0), 6)
+    
+    elapsed_time = time.time() - starting_time
+    fps = frame_id / elapsed_time
+    cv2.putText(frame, "FPS: " + str(round(fps, 2)), (10, 50), font, 4, (0, 0, 0), 3)
+    cv2.imshow("Image", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
